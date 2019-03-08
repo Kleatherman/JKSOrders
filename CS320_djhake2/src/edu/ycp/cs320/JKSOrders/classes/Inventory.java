25870d837e60edd726862c2f0d5cfbedeb71c285
Josiah Sam <jsam@ycp.edu>
2019-03-08 12:06:10 -0500

    Added a set item quantity method to inventory

--- CS320_djhake2/src/edu/ycp/cs320/JKSOrders/classes/Inventory.java
+++ CS320_djhake2/src/edu/ycp/cs320/JKSOrders/classes/Inventory.java
@@ -4,16 +4,23 @@ import java.util.ArrayList;
 	import java.util.Map;
 
 	public class Inventory {
-	private Map<String, Integer> quanityMap;
+	private Map<String, Integer> quantityMap;
 
+	public Inventory() {
+		quantityMap = new Map<String, Integer>();
+	}
 	public Map<String, Integer> getQuanityMap() {
-		return quanityMap;
+		return quantityMap;
 	}
 
 	public void setQuanityMap(Map<String, Integer> quanityMap) {
-		this.quanityMap = quanityMap;
+		this.quantityMap = quanityMap;
 	}
 
+	public void setItemQuantity(String item, Integer i) {
+		quantityMap.put(item, i);
+	}
+	
 	public ArrayList<String> returnGreaterorLess(int x, ArrayList<String> greater, ArrayList<String> less) {
 		return null;
 	}
