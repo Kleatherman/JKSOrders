fbeb486169027e4ccfe702a50184594eac890ffb
Samuel Cesario <scesario@ycp.edu>
2019-03-22 15:52:39 -0400

    Templates for Controllers added

---
+++ CS320_JKSOrders/src/edu/ycp/cs320/JKSOrders/controller/CreateEmployeeAccountController.java
@@ -0,0 +1,18 @@
+package edu.ycp.cs320.JKSOrders.controller;
+
+
+
+
+import edu.ycp.cs320.JKSOrders.model.CreateEmployeeAccount;
+
+
+
+public class CreateEmployeeAccountController {
+	private CreateEmployeeAccount model;
+	
+		
+	
+		public void setModel( CreateEmployeeAccount model) {
+			this.model = model;
+		}
+	}
