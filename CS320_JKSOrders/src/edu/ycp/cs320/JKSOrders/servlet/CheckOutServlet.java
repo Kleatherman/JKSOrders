72851517ee92de5be5a133527a2f7e4c8bf9e883
scesario <scesario@KEC132-16.ycp.edu>
2019-03-28 12:11:53 -0400

    Model/Controller passed to checkout servlet

--- CS320_JKSOrders/src/edu/ycp/cs320/JKSOrders/servlet/CheckOutServlet.java
+++ CS320_JKSOrders/src/edu/ycp/cs320/JKSOrders/servlet/CheckOutServlet.java
@@ -32,6 +32,7 @@ public class CheckOutServlet extends HttpServlet {
 
 		System.out.println("CheckOut Servlet: doPost");
 		Database db = InitDatabase.init();
+		
 		// create model - model does not persist between requests
 		// must recreate it each time a Post comes in
 		CheckOut model = new CheckOut();
