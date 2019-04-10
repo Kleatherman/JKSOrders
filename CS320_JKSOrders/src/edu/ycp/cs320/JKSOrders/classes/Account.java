c7b1d590360c9e73babd000e060e56a8f3ce7c97
Josiah Sam <jsam@ycp.edu>
2019-04-09 22:06:05 -0400

    Changed Account Class to include phone number and email

--- CS320_JKSOrders/src/edu/ycp/cs320/JKSOrders/classes/Account.java
+++ CS320_JKSOrders/src/edu/ycp/cs320/JKSOrders/classes/Account.java
@@ -8,7 +8,28 @@ public abstract class Account {
 	private LoginInfo login;
 	private PickUpInfo pickUpInfo;
 	private ArrayList<Order> orders;
-	
+	private String phoneNumber;
+	private String email;
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
 	public Account() {
 	
 	}
