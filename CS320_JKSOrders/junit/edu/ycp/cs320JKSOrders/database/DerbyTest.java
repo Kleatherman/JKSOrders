1a97c2b8552a9fa27c59f01414cf6e608f432c3f
Kyle <kleatherman1@ycp.edu>
2019-04-22 19:04:11 -0400

    New init database and extending tests

--- CS320_JKSOrders/junit/edu/ycp/cs320JKSOrders/database/DerbyTest.java
+++ CS320_JKSOrders/junit/edu/ycp/cs320JKSOrders/database/DerbyTest.java
@@ -1,21 +1,46 @@
 package edu.ycp.cs320JKSOrders.database;
 
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import edu.ycp.cs320.JKSOrders.classes.Account;
+import edu.ycp.cs320.JKSOrders.classes.EmployeeAccount;
 import edu.ycp.cs320.JKSOrders.database.Database;
+import edu.ycp.cs320.JKSOrders.database.InitDerbyDatabase;
 
 public class DerbyTest {
 	Database db;
-	ArrayList<EmployeeAccount> 
+	ArrayList list;
 	
 	@Before
 	public void setUp() {
-		db= new DerbyDatabase();
+		db = new InitDerbyDatabase().init();
 		
 	}
 	@Test 
 	public void testGetEmployeeAccounts() {
-		db.getEmployeeAccounts()
+		list= db.getEmployeeAccounts();
+		assertTrue(list.size()==3);
+		assertTrue(((Account) list.get(0)).getAccountNumber().equals("M0"));
+	}
+	@Test 
+	public void testGetCustomersAccounts() {
+		list= db.getCustomerAccounts();
+		assertTrue(list.size()==3);
+		assertTrue(((Account) list.get(0)).getAccountNumber().equals("C0"));
+	}
+	@Test
+	public void testGetCustomerLogin() {
+		list= db.getCustomerLoginInfo();
+		assertTrue(list.size()==3);
+		assertTrue(list.get(0).);
+	}
+	@Test
+	public void testGetEmployeeLogin() {
+		
 	}
 }
---
+++ CS320_JKSOrders/src/edu/ycp/cs320/JKSOrders/database/InitDerbyDatabase.java
@@ -0,0 +1,8 @@
+package edu.ycp.cs320.JKSOrders.database;
+
+public class InitDerbyDatabase {
+	public static Database init() {
+		return new DerbyDatabase();
+	}
+
+}
